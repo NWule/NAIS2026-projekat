d679b63724af320ac8e35c04bbd0299eec8c7f27
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-04-26 17:45:38 +0200

    feat: Initialized new service.

---
+++ PlayerRecommendationService/src/test/java/NAIS/PlayerRecommendationService/PlayerRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package NAIS.PlayerRecommendationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
