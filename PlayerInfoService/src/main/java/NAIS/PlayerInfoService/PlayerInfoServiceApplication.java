ac81b6428ea1637dd7c86920a37570c8f493c6ee
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-05-30 23:45:56 +0200

    feat: Initialized PlayerInfoService

---
+++ PlayerInfoService/src/main/java/NAIS/PlayerInfoService/PlayerInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package NAIS.PlayerInfoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerInfoServiceApplication.class, args);
+	}
+
+}
