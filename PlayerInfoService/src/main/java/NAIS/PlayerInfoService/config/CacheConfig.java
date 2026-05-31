6a9038a86b2e6bdb4e300c84f9127aa2ce270787
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-05-31 15:00:19 +0200

    feat: Added redis and redis insight. Added cache config to PlayerInfoService.

---
+++ PlayerInfoService/src/main/java/NAIS/PlayerInfoService/config/CacheConfig.java
@@ -0,0 +1,40 @@
+package NAIS.PlayerInfoService.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        cacheConfigurations.put("playerTiersCache", defaultCacheConfig.entryTtl(Duration.ofHours(1)));
+
+        cacheConfigurations.put("tacticalTrendsCache", defaultCacheConfig.entryTtl(Duration.ofMinutes(30)));
+
+        cacheConfigurations.put("mediaRiskCache", defaultCacheConfig.entryTtl(Duration.ofHours(1)));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultCacheConfig)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+    }
+}
