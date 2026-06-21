b9e1fd98ff5188529eb83202890fcd06305ddd28
Luka Kojović <kojovic.ra107.2022@uns.ac.rs>
2026-06-21 04:23:50 +0200

    feat: Added event for report creation confirmation. Added a listener for report creation compensation.

---
+++ PlayerInfoService/src/main/java/NAIS/PlayerInfoService/saga/CompensationListener.java
@@ -0,0 +1,37 @@
+package NAIS.PlayerInfoService.saga;
+
+import NAIS.PlayerInfoService.config.RabbitMQConfig;
+import NAIS.PlayerInfoService.repository.ReportRepository;
+import NAIS.PlayerInfoService.saga.event.ReportCreationConfirmationEvent;
+import NAIS.PlayerInfoService.saga.event.ReportCreationFailedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CompensationListener {
+    private  final ReportRepository reportRepo;
+
+    @RabbitListener(queues = RabbitMQConfig.REPORT_CREATED_CONFIRMATION_QUEUE)
+    public void handleCreationOutcome(Object event) {
+        if (event instanceof ReportCreationFailedEvent failedEvent) {
+            try {
+                if (reportRepo.existsById(failedEvent.getReportId())) {
+                    reportRepo.deleteById(failedEvent.getReportId());
+                    log.warn("Report creation failed for report ID: " + failedEvent.getReportId() + ", report deleted.");
+                } else {
+                    log.warn("Report creation failed for report ID: " + failedEvent.getReportId() + ", report not found.");
+                }
+            } catch (Exception e) {
+                log.error("Error while trying to delete report: " + e.getMessage(), e);
+            }
+        } else if (event instanceof ReportCreationConfirmationEvent confirmationEvent) {
+            log.info("Report creation confirmed for report ID: " + confirmationEvent.getReportId());
+        } else {
+            log.warn("Unknown event type: " + event.getClass().getName());
+        }
+    }
+}
